1df75d42cd183bbfab381ef26f95079c4a8b6e9d
VForiel <vince.lrf@gmail.com>
2025-09-29 12:42:55 +0200

    Essential fix

--- scripts/cli_linux.py
+++ scripts/cli_linux.py
@@ -69,7 +69,7 @@ def main():
 #==============================================================================
 
 def show_help():
-    print("📋 KBENCH - Kernel Bench Control Interface")
+    print("📋 KBENCH - Ker... euh, Photonics Bench Control Interface")
     print("="*50)
     print("Usage: kbch [equipment] [command] [options]")
     print("       kbch config [command] [options]")
